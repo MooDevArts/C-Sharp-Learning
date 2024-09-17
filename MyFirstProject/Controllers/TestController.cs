55619a82ec9307279ca054abe1ad0f981c0d91f3
Himanshu <himanshu.a.04.1999@gmail.com>
2024-09-17 14:54:07 -0400

    Initial commit

---
+++ MyFirstProject/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TestController : ControllerBase
+    {
+        [HttpGet(template:"test1")]
+        public string Method1() => "Hello Humber";
+
+        [HttpGet(template: "test2")]
+        public string Method2()
+        {
+            return "Hello 5125 in Humber";
+        }
+    }
+}
